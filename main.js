fe1ce3f47fcc5c6adb01823ee14d5d020d9402a7
Perseus289 <85447860+Perseus289@users.noreply.github.com>
2022-02-13 19:38:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function draw(){
+
+}
+function setup(){
+    canvas = createCanvas(300 , 300);
+    canvas.center();
+}
+function preload(){
+
+}
+function take_snapshot(){
+    save('Mustachefilter.png');
+}
